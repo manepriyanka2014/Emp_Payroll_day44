032b0fe063d66153cfc13650a02ec60d893e36b8
manepriyanka2014 <manepriyanka2014@gmail.com>
2022-01-04 22:59:31 +0530

    Ability to crete an Employee Payroll HomePage

--- JS/PayrollForm.js
+++ JS/PayrollForm.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
       return;
     }
     try {
-      (new EmployeePayrollData())._name = name.value;
+      (new EmployeePayrollData()).name = name.value;
       textError.textContent = "";
     } catch (e) {
       textError.textContent = e;

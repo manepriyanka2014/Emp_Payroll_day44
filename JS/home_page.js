1b64192f9bc670875da428f6f7725b43e4a6837d
manepriyanka2014 <manepriyanka2014@gmail.com>
2022-01-05 10:38:36 +0530

    Ability to view employee payroll details in a Tabular format from JSON object

--- JS/home_page.js
+++ JS/home_page.js
@@ -3,26 +3,71 @@ window.addEventListener('DOMContentLoaded', (event) => {
   });
   
   createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
-                       "<th>Salary</th><th>StartDate</th><th>Actions</th>";
-  
-    const innerHtml = `${headerHtml}
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+      "<th>Salary</th><th>StartDate</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+      innerHtml = `${innerHtml}
     <tr>
-      <td class="profile"><img alt="" src="..\Assets\profile\Ellipse -5.png">
+      <td class="profile"><img alt="" src="${empPayrollData._profilePic}">
       </td>
-      <td>Madhukar Tatiraju</td>
-      <td>Male</td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td>${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Finance</div>
-      </td>
-      <td>300000</td>
-      <td>1 Nov 2020</td>
-      <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="E:\HTML\Day_44_AddEmployee_payroll\Assets\icons\delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" src="E:\HTML\Day_44_AddEmployee_payroll\Assets\icons\create-black-18dp.svg">
+        <img name="${empPayrollData._id}" onclick="remove(this)" src="..\Assets\icons\delete-black-18dp.svg" alt="delete">
+        <img name="${empPayrollData._id}" onclick="update(this)" src="..\Assets\icons\create-black-18dp.svg" alt="edit">
       </td>
     </tr>
       `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+  }
+  
+  const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+      {
+        _name: "Narayan Mahadevan",
+        _gender: 'Male',
+        _department: ['Engineer', 'Sales'],
+        _salary: '500000',
+        _startDate: '29 oct 2019',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '..\Assets\profile\Ellipse -2.png'
+      },
+      {
+        _name: 'Priyanka Mane',
+        _gender: 'Female',
+        _department: ['Engineer', 'Finance'],
+        _salary: '350000',
+        _startDate: '6 Nov 2021',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '..\Assets\profile\Ellipse -8.png'
+      },
+      {
+        _name: 'Avinash Jadhav',
+        _gender: 'Male',
+        _department: ['HR', 'Finance'],
+        _salary: '450000',
+        _startDate: '30 Sep 2016',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '..\Assets\profile\Ellipse -3.png'
+      }
+  
+    ];
+    return empPayrollListLocal;
+  };
+  
+  const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+      deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
   }
--- JS/site_properties.js
+++ JS/site_properties.js
@@ -1,4 +1,4 @@
 let site_properties = {
-    home_page: "../pages/homePage.html",
-    add_emp_payroll_page: "../pages/EmployeePayrollApp.html"
+    home_page: "..\HomePage.html",
+    add_emp_payroll_page: "..\EmployeePayrollApp.html"
   };
